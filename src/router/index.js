820592de12a9641d847a6b84acf0c9e1b1ea9f1c
Walid <elouardiwalid99@gmail.com>
2023-09-12 16:18:39 +0100

    orgnise the router.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,26 +6,26 @@ const router = createRouter({
   routes: [
     {
       path: "/",
+      name: "home",
       beforeEnter: (to, from, next) => {
         useUserStore().id ? next("/profile") : next();
       },
-      name: "home",
       component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/login",
+      name: "login",
       beforeEnter: (to, from, next) => {
         useUserStore().id ? next("/profile") : next();
       },
-      name: "login",
       component: () => import("../views/LoginView.vue"),
     },
     {
       path: "/register",
+      name: "register",
       beforeEnter: (to, from, next) => {
         useUserStore().id ? next("/profile") : next();
       },
-      name: "register",
       component: () => import("../views/RegisterView.vue"),
     },
     {
@@ -33,53 +33,58 @@ const router = createRouter({
       beforeEnter: (to, from, next) => {
         useUserStore().id ? next() : next("/login");
       },
-      name: "profile",
-      component: () => import("../views/account/ProfileSection.vue"),
-    },
-    {
-      path: "/profile/edit-profile",
-      name: "edit-profile",
-      component: () => import("../views/account/EditProfileSection.vue"),
-    },
-    {
-      path: "/profile/add-song",
-      name: "add-song",
-      component: () => import("../views/account/AddSong.vue"),
-    },
-    {
-      path: "/profile/delete-song",
-      name: "delete-song",
-      component: () => import("../views/account/deleteSong.vue"),
-    },
-    {
-      path: "/profile/add-youtube-video",
-      name: "add-youtube-video",
-      component: () => import("../views/account/AddYoutubeVideo.vue"),
-    },
-    {
-      path: "/profile/delete-youtube-video",
-      name: "delete-youtube-video",
-      component: () => import("../views/account/DeleteYoutubeVideo.vue"),
-    },
-    {
-      path: "/profile/create-post",
-      name: "create-post",
-      component: () => import("../views/account/CreatePost.vue"),
-    },
-    {
-      path: "/profile/edit-post/:id",
-      name: "edit-post",
-      component: () => import("../views/account/EditPost.vue"),
-    },
-    {
-      path: "/profile/post/:id",
-      name: "post",
-      component: () => import("../views/account/PostsById.vue"),
-    },
-    {
-      path: "/profile/posts",
-      name: "posts",
-      component: () => import("../views/account/Posts.vue"),
+      children: [
+        {
+          path: "",
+          name: "profile",
+          component: () => import("../views/account/ProfileSection.vue"),
+        },
+        {
+          path: "edit-profile",
+          name: "edit-profile",
+          component: () => import("../views/account/EditProfileSection.vue"),
+        },
+        {
+          path: "add-song",
+          name: "add-song",
+          component: () => import("../views/account/AddSong.vue"),
+        },
+        {
+          path: "/profile/delete-song",
+          name: "delete-song",
+          component: () => import("../views/account/deleteSong.vue"),
+        },
+        {
+          path: "/profile/add-youtube-video",
+          name: "add-youtube-video",
+          component: () => import("../views/account/AddYoutubeVideo.vue"),
+        },
+        {
+          path: "/profile/delete-youtube-video",
+          name: "delete-youtube-video",
+          component: () => import("../views/account/DeleteYoutubeVideo.vue"),
+        },
+        {
+          path: "/profile/create-post",
+          name: "create-post",
+          component: () => import("../views/account/CreatePost.vue"),
+        },
+        {
+          path: "/profile/edit-post/:id",
+          name: "edit-post",
+          component: () => import("../views/account/EditPost.vue"),
+        },
+        {
+          path: "/profile/post/:id",
+          name: "post",
+          component: () => import("../views/account/PostsById.vue"),
+        },
+        {
+          path: "/profile/posts",
+          name: "posts",
+          component: () => import("../views/account/Posts.vue"),
+        },
+      ],
     },
   ],
 });
